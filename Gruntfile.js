c513716769a0753f106a9add07973de6ac753ffe
Jeff Ma <mjf429@163.com>
2014-04-24 00:05:25 +0800

    change grunt sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -115,7 +115,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-coffeelint');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-ember-handlebars');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
